4df8a6ba18b04c7f749b0866ef45ae7d832930ce
Jonas Carvalho <jonasgabrielsc@gmail.com>
2025-04-01 23:21:54 -0300

    Projeto upado

---
+++ src/main/java/com/belaArtes/demo/ApiBelaArtesApplication.java
@@ -0,0 +1,13 @@
+package com.belaArtes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBelaArtesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBelaArtesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belaArtes/demo/ApiBelaArtesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belaArtes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBelaArtesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
